657d98c397fd74ff66ffc689091e1a7c3884269b
Gary Lake <zero303@gmail.com>
2022-08-18 16:40:51 +0100

    Fix local registration of component not working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import DynamicHeroicon from './components/DynamicHeroicon.vue';
 
-export default {
-  install: (app, options) => {
-    app.component('DynamicHeroicon', DynamicHeroicon);
-  },
-}
+export { DynamicHeroicon };
